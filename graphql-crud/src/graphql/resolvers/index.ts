1145e90730261acaa4a8e3c075d247954e31ecdd
rusiruavb <rusiruavb98@gmail.com>
2022-06-25 11:53:07 +0530

    bug: sequelize association problem

--- graphql-crud/src/graphql/resolvers/index.ts
+++ graphql-crud/src/graphql/resolvers/index.ts
@@ -1,6 +1,7 @@
 import { mergeResolvers } from "@graphql-tools/merge";
 import { userResolver } from "./user_resolver";
+import { toDoResolver } from "./todo_resolver";
 
-const resolvers = [userResolver];
+const resolvers = [userResolver, toDoResolver];
 
 export default mergeResolvers(resolvers);
--- graphql-crud/src/models/ToDo.ts
+++ graphql-crud/src/models/ToDo.ts
@@ -6,6 +6,7 @@ const ToDo = sequelize.define(
   "ToDo",
   {
     description: { type: DataTypes.STRING(500) },
+    status: { type: DataTypes.STRING },
   },
   {
     paranoid: true,
